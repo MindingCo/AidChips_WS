86e819bba1d896f817a5da2a7a898307ee84875f
M1ndBlast <gustavopdzz0@gmail.com>
2019-05-07 11:13:44 -0500

    añadiendo todo el crud, solo falta receivedpermits y givenpermits

--- modules/SQLController.js
+++ modules/SQLController.js
@@ -3,79 +3,206 @@ const mysql = require('mysql');
 const config = require('./SQLConfig');
 const connection = mysql.createConnection(config);
 
-// sqlController.finish = connection.end();
+function getIdUserByName(name, cb)
+{
+    connection.query('SELECT id_usu FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
+    {
+        if (error) throw error;
+        else // execute callback 'cuz without cb return a undefined value (async)
+            cb(result.id_usu)
+    })
+}
+function getIdChipByNSerie(nSerie, cb)
+{
+    connection.query('SELECT id_chp FROM chip WHERE nse_chp = ? LIMIT 1', [nSerie], (error, result) =>
+    {
+        if (error) throw error;
+        else // execute callback 'cuz without cb return a undefined value (async)
+            cb(result.id_chp)
+    })
+}
 function exitsUser(name, cb)
 {
-   connection.query('SELECT  nmc_usu FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
-   {
-      if (error) throw error;
-      else // execute callback 'cuz without cb return a undefined value (async)
-         cb(!(result.length === 0))
-   });
+    connection.query('SELECT nmc_usu FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
+    {
+        if (error) throw error;
+        else // execute callback 'cuz without cb return a undefined value (async)
+            cb(!(result.length === 0))
+    })
 }
+function exitsChip(nSerie, cb)
+{
+    connection.query('SELECT nse_chp FROM chip WHERE nse_chp = ? LIMIT 1', [nSerie], (error, result) =>
+    {
+        if (error) throw error;
+        else // execute callback 'cuz without cb return a undefined value (async)
+            cb(!(result.length === 0))
+    })
+}
+function exitsPermit(userId, chipId, typePermit, cb)
+{
+    connection.query('SELECT id_usu FROM permit WHERE id_usu = ? id_chp = ? and and pro_per = ? LIMIT 1', [userId, chipId, typePermit], (error, result) =>
+    {
+        if (error) throw error;
+        else // execute callback 'cuz without cb return a undefined value (async)
+            cb(!(result.length === 0))
+    })
+}
+
 sqlController.addUser = (req, res) =>
 {
-   const name = req.body.name;
-   const email = req.body.email;
-   const tel = req.body.tel;
-   const image = req.body.image;
-   const pass = req.body.pass;
+    const name = req.body.name;
+    const email = req.body.email;
+    const tel = req.body.tel;
+    const image = req.body.image;
+    const pass = req.body.pass;
 
-   exitsUser(name, (exits) =>
-   {
-      if (exits) res.send(false);
-      else
-         connection.query('INSERT INTO user VALUES(0, ?, ?, ?, ?, ?)', [name, email, tel, (image != null)? image:null, pass], error =>
-         {
-            if (error) throw error
-            else res.send(true)
-         });
-   });
+    exitsUser(name, (exits) =>
+    {
+        if (exits) res.send(false);
+        else
+            connection.query('INSERT INTO user VALUES(0, ?, ?, ?, ?, ?)', [name, email, tel, (image != null)? image:null, pass], error =>
+            {
+                if (error) throw error;
+                else res.send(true)
+            })
+    })
 };
 sqlController.getUserById = (req, res) =>
 {
-   const id = req.body.id;
-   connection.query('SELECT  id_usu, nmc_usu, ema_usu, cel_usu, img_usu FROM user WHERE id_usu = ? LIMIT 1', [id], (error, result) =>
-   {
-      if (error) throw error
-      res.send((result.length === 0) ? null : result[0])
-   });
+    const id = req.body.id;
+    connection.query('SELECT id_usu, nmc_usu, ema_usu, cel_usu, img_usu FROM user WHERE id_usu = ? LIMIT 1', [id], (error, result) =>
+    {
+        if (error) throw error;
+        res.send((result.length === 0) ? null : result[0])
+    })
 };
 sqlController.getUserByName = (req, res) =>
 {
-   const name = req.body.name;
-   connection.query('SELECT  id_usu, nmc_usu, ema_usu, cel_usu, img_usu FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
-   {
-      if (error) throw error
-      res.send((result.length === 0) ? null : result[0])
-   });
+    const name = req.body.name;
+    connection.query('SELECT id_usu, nmc_usu, ema_usu, cel_usu, img_usu FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
+    {
+        if (error) throw error;
+        res.send((result.length === 0) ? null : result[0])
+    })
 };
 sqlController.getUserForLogin = (req, res) =>
 {
-   const name = req.body.name;
-   connection.query('SELECT * FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
-   {
-      if (error) throw error
-      res.send((result.length === 0) ? null : result[0])
-   });
+    const name = req.body.name;
+    connection.query('SELECT * FROM user WHERE nmc_usu = ? LIMIT 1', [name], (error, result) =>
+    {
+        if (error) throw error;
+        res.send((result.length === 0) ? null : result[0])
+    })
 };
 sqlController.delUserById = (req, res) =>
 {
-   const id = req.body.id;
-   connection.query('DELETE FROM user WHERE id_usu = ?', [id], error =>
-   {
-      if (error) throw error
-      else res.send(true)
-   });
+    const id = req.body.id;
+    connection.query('DELETE FROM user WHERE id_usu = ?', [id], error =>
+    {
+        if (error) throw error;
+        else res.send(true)
+    })
 };
 sqlController.delUserByName = (req, res) =>
 {
-   const name = req.body.name;
-   connection.query('DELETE FROM user WHERE nmc_usu = ?', [name], error =>
-   {
-      if (error) throw error
-      else res.send(true)
-   });
+    const name = req.body.name;
+    connection.query('DELETE FROM user WHERE nmc_usu = ?', [name], error =>
+    {
+        if (error) throw error;
+        else res.send(true)
+    })
 };
+
+sqlController.addChip = (req, res) =>
+{
+    const nserie = req.body.nserie;
+    const ownName = req.body.ownName;
+    const tel = req.body.tel;
+
+    exitsChip(nserie, (exits) =>
+    {
+        if (exits) res.send(false);
+        else
+            connection.query('INSERT INTO chip VALUES(0, ?, ?, ?, 1)', [nserie, ownName, tel], error =>
+            {
+                if (error) throw error;
+                else res.send(true)
+            })
+    })
+};
+sqlController.getChip = (req, res) =>
+{
+    connection.query('SELECT id_chp, nse_chp, npr_chp, cel_chp FROM chip WHERE nse_chp = ? and act_chp = 1 LIMIT 1', [req.body.nserie], (error, result) =>
+    {
+        if (error) throw error;
+        else res.send((result.length === 0) ? null : result[0])
+    })
+};
+sqlController.delChip = (req, res) =>
+{
+    connection.query('DELETE FROM chip WHERE nse_chp = ?', [req.body.nserie], error =>
+    {
+        if (error) throw error;
+        else res.send(true)
+    })
+};
+sqlController.getChips = (req, res) =>
+{
+    // SELECT id_chp, nse_chp, npr_chp, cel_chp FROM permit NATURAL JOIN chip NATURAL JOIN user WHERE id_usu = ? and pro_per = ? and act_per = 1
+    connection.query('SELECT id_chp, nse_chp, npr_chp, cel_chp FROM permit NATURAL JOIN chip WHERE id_usu = ? and pro_per = ? and act_per = 1', [req.body.id, req.body.owner===true? 1:0], (error, result)  =>
+    {
+        if (error) throw error;
+        res.send((result.length === 0) ? null : result)
+    })
+};
+
+sqlController.addPermit = (req, res) =>
+{
+    const id = req.body.id;
+    const nSerie = req.body.nserie;
+    const owner = req.body.owner; //    should be boolean
+
+    getIdChipByNSerie(nSerie, idChip =>
+    {
+        exitsPermit(id, idChip, owner===true? 1:0, (exits) =>
+        {
+            if (exits) res.send(false);
+            else
+                connection.query('INSERT INTO permit VALUES(0, ?, ?, ?, 1)', [id, idChip, owner===true? 1:0], error =>
+                {
+                    if (error) throw error;
+                    else res.send(true)
+                })
+        })
+    })
+};
+sqlController.getPermit = (req, res) =>
+{
+    connection.query('SELECT id_per, id_usu, id_chp, pro_per FROM permit WHERE id_usu = ? and id_chp = ? and pro_per = ? LIMIT 1', [req.body.id, req.body.nserie, req.body.owner===true? 1:0], (error, result) =>
+    {
+        if (error) throw error;
+        res.send((result.length === 0) ? null : result[0])
+    })
+};
+sqlController.delPermit = (req, res) =>
+{
+    getIdChipByNSerie(req.body.nserie, idChip =>
+    {
+        connection.query('DELETE FROM permit WHERE id_usu = ? and id_chp = ? and pro_per = ?', [req.body.id, idChip, req.body.owner===true? 1:0], error =>
+        {
+            if (error) throw error;
+            else res.send(true)
+        })
+    })
+};
+// sqlController.getReceivedPermits = (req, res) =>
+// {
+//     connection.query('SELECT nse_chp, id_usu, nmc_usu, tel_usu, img_usu from permit ', [req.body.id, idChip, req.body.owner===true? 1:0], error =>
+//     {
+//         if (error) throw error;
+//         else res.send(true)
+//     })
+// };
 
 module.exports = sqlController;
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,20 @@ const express = require('express');
 const router = express.Router();
 const sqlController = require('../modules/SQLController');
 
-/* GET home page. */
 router.get('/', (req, res) =>
-{
-    res.render('index', { title: 'Express' });
-});
+    res.render('index', { title: 'Express' })
+);
+//  User Crud
 router.post('/login', (req, res) =>
     sqlController.getUserForLogin(req, res) );
+
 router.post('/add/user', (req, res) =>
     sqlController.addUser(req, res) );
 router.post('/get/user', (req, res) =>
 {
-    const id = req.body.id;
-    const name = req.body.name;
-
-    if (id !== undefined)
+    if (req.body.id !== undefined)
         sqlController.getUserById(req, res);
-    else if (name !== undefined)
+    else if (req.body.name !== undefined)
         sqlController.getUserByName(req, res);
     else
         res.send({message: "Envia parametros"})
@@ -26,9 +23,26 @@ router.post('/get/user', (req, res) =>
 router.post('/del/user', (req, res) =>
 {
     if (req.body.id !== undefined)
-        sqlController.delUserById(req, res)
+        sqlController.delUserById(req, res);
     else
         sqlController.delUserByName(req, res)
 });
+//  Chip Crud
+router.post('/add/chip', (req, res) =>
+    sqlController.addChip(req, res) );
+router.post('/get/chip', (req, res) =>
+    sqlController.getChip(req, res) );
+router.post('/del/chip', (req, res) =>
+    sqlController.delChip(req, res) );
+router.post('/get/chips', (req, res) =>
+    sqlController.getChips(req, res) );
+
+//  Permit Crud
+router.post('/add/permit', (req, res) =>
+    sqlController.addPermit(req, res) );
+router.post('/get/permit', (req, res) =>
+    sqlController.getPermit(req, res) );
+router.post('/del/permit', (req, res) =>
+    sqlController.delPermit(req, res) );
 
 module.exports = router;
