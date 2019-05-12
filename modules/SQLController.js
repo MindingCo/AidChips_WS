db8739f35082048ea051bf68c4df39080a3371b7
M1ndBlast <gustavopdzz0@gmail.com>
2019-05-12 01:04:36 -0500

    finalizacion completa para la funcion añadir chip y permiso

--- modules/SQLController.js
+++ modules/SQLController.js
@@ -16,7 +16,6 @@ function getIdChipByNSerie(nSerie, cb)
 {
     connection.query('SELECT id_chp FROM chip WHERE nse_chp = ? LIMIT 1', [nSerie], (error, result) =>
     {
-        console.log(result[0].id_chp);
         if (error) throw error;
         else // execute callback 'cuz without cb return a undefined value (async)
             cb(result[0].id_chp)
